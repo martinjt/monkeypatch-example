146d5f06281c0129548ce51fcc77092802c3c5ea
Martin Thwaites <github@my2cents.co.uk>
2022-05-01 22:53:58 +0100

    Phase 2 - switch to a global using

---
+++ src/monkeypatch-test/Globals.cs
@@ -0,0 +1,1 @@
+global using Console = monkeypatch_test.PrefixConsole;
--- src/monkeypatch-test/Program.cs
+++ src/monkeypatch-test/Program.cs
@@ -1,4 +1,2 @@
-﻿using Console = monkeypatch_test.PrefixConsole;
-
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
